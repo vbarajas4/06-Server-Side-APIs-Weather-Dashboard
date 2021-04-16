8d1ec4b10ec0cad32b023e4b50208c7fb94d849c
Vanessa Maldonado <vbarajas4@gmail.com>
2021-04-16 00:03:36 -0700

    finished html and css started js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,26 @@
-fetch("https://api.openweathermap.org/data/2.5/weather?q=San Diego&appid=83f513b8e9c0bfde2e896c17f85639ab").then(function(response){
-    console.log(response)
-    return response.json()
-}).then(function(data){
-
-var lat = data.coord.lat
-var lon = data.coord.lon
-
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid=83f513b8e9c0bfde2e896c17f85639ab").then(function(response){
-    return response.json()
-}).then(function(data){
-    console.log(data)
-})
-})
+var button = document.querySelector('#search-btn')
+var searchCity = document.querySelector('#search-city')
+var cityName = document.querySelector('#city-name')
+var currentPic = document.querySelector('#current-pic')
+var temperature = document.querySelector('#temperature')
+var humidity = document.querySelector('#humidity')
+var windSpeed = document.querySelector('#wind-speed')
+var UV = document.querySelector('#UV-index')
 
 
+
+
+    button.addEventListener('click', function(){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=83f513b8e9c0bfde2e896c17f85639ab")
+        .then(function (response) {
+          return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+      //  .catch(function (err) {
+      //      alert("Wrong city name!")
+     //   })
+    })
 
 
